0e71889ebb71e8eaaed909c6b0f9b99fcd82166e
Genaro Tapia <genaro@sicar.mx>
2024-10-31 09:27:14 -0600

    gradle updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,31 @@ version = "1.0.0"
 
 gradlePlugin {
     plugins {
-        create("versioningPlugin") {
-            id = "com.sicarx.versioning"
+        create("listenerPlugin") {
+            id = "sxt-plugin"
             implementationClass = "com.sicarx.TestListenerPlugin"
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            from(components["java"])
+            groupId = project.group.toString()
+            artifactId = "sxt-plugin"
+            version = project.version.toString()
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/GenaroTapia/sxt-plugin")
+            credentials {
+                username = System.getenv("MAVEN_USER")
+                password = System.getenv("MAVEN_SECRET")
+            }
         }
     }
 }
